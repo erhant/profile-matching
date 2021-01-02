1a01c1aaeb8bd50474dfbc9cce4e591cbd152bdd
Waris Gill <wgill18@ku.edu.tr>
2021-01-03 02:12:22 +0300

    scoring functions are added

--- src/matcher.py
+++ src/matcher.py
@@ -4,13 +4,20 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from sentence_transformers import SentenceTransformer, util
 import torch
-import tensorflow as tf
+# import tensorflow as tf
 from SSIM_PIL import compare_ssim
 from PIL import Image
 from skimage.measure import compare_ssim
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import nltk
+from nltk.util import ngrams
+
 import numpy as np
 import cv2
+from pyjarowinkler import distance
+
+from strsimpy.cosine import Cosine
+
 
 
 
@@ -95,6 +102,48 @@ def textSemantic(text=None): # maybe compare headline with tweets?
     r_dict = classifier(text)[0]
     return r_dict["label"],r_dict["score"] 
 
+
+def usernameSimilarityScore(uname1, uname2):
+    """
+        Return score between 0 and 1. 
+    """
+ 
+    # score = nltk.edit_distance(uname1, uname2,transpositions=True)
+    # score = 1-nltk.jaccard_distance(set(ngrams('waris.gill', 2)), set(ngrams('gill.waris', 2)))
+    
+    score = distance.get_jaro_distance(uname1,uname2,winkler=False)
+    
+    return score
+
+def locationSimilarityScore(l1, l2):
+
+    if l1 == l2:
+        return 1 # matched exatcly
+    else:
+        return 0
+
+def namedEntityRecogntion(text):
+    ner = pipeline("ner")
+    output = ner(text)
+    
+    result = [d["word"] for d in output]
+    return result
+
+def cosineSimilarityScore(l1,l2):
+    d1 = {k:1 for k in l1}
+    d2 = {k:1 for k in l2}
+    cosine = Cosine(2)
+
+
+    score =  cosine.similarity_profiles(d1,d2)
+    return score
+
+    
+
+
+
+
+
 def findNaiveSimilar():
     return 1 # todo
 
@@ -109,24 +158,55 @@ class Matcher:
         return 1 # todo
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
-    label, score = textSemantic("hello! I am not happy")
-    print(f"\n>Text Semantic Score: {score} and label: {label} ")
-    text= ' New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.  A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.  Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.  In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.  Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the  2010 marriage license application, according to court documents.  Prosecutors said the marriages were part of an immigration scam.  On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further.  After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective  Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002.  All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say.  Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages.  Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted.  The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s  Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.  Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force.  If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18. '
-    summary = textSummarization(text)
-    print(f"\n>Text Summary: {summary}" )
+#     # label, score = textSemantic("hello! I am not happy")
+#     # print(f"\n>Text Semantic Score: {score} and label: {label} ")
+#     text= ' New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.  A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.  Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.  In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.  Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the  2010 marriage license application, according to court documents.  Prosecutors said the marriages were part of an immigration scam.  On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further.  After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective  Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002.  All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say.  Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages.  Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted.  The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s  Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.  Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force.  If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18. '
+#     summary = textSummarization(text)
+#     # print(f"\n>Text Summary: {summary}" )
 
-    score = textSimilarity(text,summary)
-    print(f"\n>Text Similarity Score: {score}")
+#     score = textSimilarity(text,summary)
+#     print(f"\n>Text Similarity Score: {score}")
 
-    score = imageSimilarity("i1.jpg","i2.jpg")
-    print(f"\n>Image Similarity Score: {score}")
+#     score = imageSimilarity("./images/i1.jpg","./images/i2.jpg")
+#     # print(f"\n>Image Similarity Score: {score}")
+#     # s1 = "waris.gill"
+#     # s2 = "gil.waris"
+#     # score = usernameSimilarityScore(s1,s2)
+#     print(f"ner 2= {namedEntityRecogntion(text)}")
+#     print(f"ner 1 = {namedEntityRecogntion(summary)}")
+   
+
+
+#     print(f"Score={score}")
+    
 
     
     
 
     
+
+
+#%%
+
+ # label, score = textSemantic("hello! I am not happy")
+    # print(f"\n>Text Semantic Score: {score} and label: {label} ")
+text= ' New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.  A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.  Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.  In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.  Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the  2010 marriage license application, according to court documents.  Prosecutors said the marriages were part of an immigration scam.  On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further.  After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective  Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002.  All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say.  Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages.  Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted.  The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s  Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.  Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force.  If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18. '
+summary = textSummarization(text)
+# print(f"\n>Text Summary: {summary}" )
+
+# score = textSimilarity(text,summary)
+# print(f"\n>Text Similarity Score: {score}")
+
+# score = imageSimilarity("./images/i1.jpg","./images/i2.jpg")
+# print(f"\n>Image Similarity Score: {score}")
+# s1 = "waris.gill"
+# s2 = "gil.waris"
+# score = usernameSimilarityScore(s1,s2)
+
+
+#%%
 
 
 # %%
